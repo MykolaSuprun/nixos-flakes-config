ed71a29f0ef0153d726346499c0b846b1ac0134f
Mykola Suprun <mykola.suprun@protonmail.com>
2023-01-14 20:35:45 +0100

    add possibility to unstable packages and overlays

--- flake.nix
+++ flake.nix
@@ -3,43 +3,54 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    home-manager.url = "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
-    home-manager.inputs.nixpkgs.follows  = "nixpkgs";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager.url =
+      "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    discord-overlay.url = "github:InternetUnexplorer/discord-overlay";
+    discord-overlay.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, home-manager, ...}: 
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, discord-overlay, ... }:
 
-  let
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-
-    lib = nixpkgs.lib; 
-
-  in {
-    nixosConfigurations = {
-      Geks-Nixos = lib.nixosSystem {
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
         inherit system;
-
-        modules = [
-          ./system/configuration.nix
+        config = { allowUnfree = true; };
+        overlays = [
         ];
       };
-    };
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config = { allowUnfree = true; };
+        overlays = [ 
+          discord-overlay.overlays.default 
+          (self: super: {
+            steam = (super.steam.override {
+              extraPkgs = pkgs-unstable: with pkgs-unstable; [ pango harfbuzz libthai ];
+            });
+          })
+        ];
+      };
+      lib = nixpkgs.lib;
 
-    homeConfigurations.mykolas = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [
-        ./users/mykolas/home.nix
-      ];
+    in {
+      nixosConfigurations = {
+        Geks-Nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./system/configuration.nix ];
+        };
+      };
 
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
+      homeConfigurations.mykolas = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./users/mykolas/home.nix ];
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = { inherit pkgs-unstable; };
+      };
     };
-  };
 }
