5e95afc5397558f60d5163bda3cda9b91d9d9da1
Mykola Suprun <mykola.suprun@protonmail.com>
2023-07-09 08:25:44 +0200

    wezterm update

--- home/mykolas/wezterm/wezterm.lua
+++ home/mykolas/wezterm/wezterm.lua
@@ -12,18 +12,27 @@ end
 
 -- This is where you actually apply your config choices
 
+config.window_decorations = 'RESIZE'
+config.display_pixel_geometry = "RGB"
+config.freetype_load_target = "Light"
+config.freetype_render_target = 'VerticalLcd'
+
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 10
-config.font = wezterm.font_with_fallback {
-  {
-    family = 'JetBrains Mono',
-    weight = 'Medium',
-    -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=1' },
-  },
-  { family = 'Terminus', weight = 'Bold' },
-  'Noto Color Emoji',
-}
+config.font = wezterm.font(
+  'JetBrainsMono Nerd Font'
+)
+-- config.font = wezterm.font_with_fallback ({
+--   "JetBrainsMono Nerd Font",
+--   {
+--     family = "Symbols Nerd Font Mono",
+--     scale = 0.75
+--     -- weight = 'Medium',
+--     -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=1' },
+--   },
+-- })
+use_cap_height_to_scale_fallback_fonts = true
 -- window size on open
 config.initial_rows = 70
 config.initial_cols = 260
