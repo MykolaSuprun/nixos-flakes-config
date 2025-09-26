6bddd2ed88c4b5222703faf6185e36aff5e399af
Mykola Suprun <mykola.suprun@protonmail.com>
2025-09-26 23:17:47 +0200

    Waybar fixes

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -4,14 +4,14 @@
     # systemd.enable = true;
     # systemd.target = "hyprland-session.target";
     # window#waybar, tooltip {
-    #     background: alpha(@base00, 1.000000);
+    #     background: alpha(@base, 1.000000);
     # }
     #
     # * { font-family: "JetBrainsMono Nerd Font";
     #     font-size: 10pt;
     # }
     #
-    # @define-color base00 #eff1f5; @define-color base01 #e6e9ef;
+    # @define-color base #eff1f5; @define-color base01 #e6e9ef;
     # @define-color base02 #ccd0da; @define-color base03 #bcc0cc;
     # @define-color base04 #acb0be; @define-color base05 #4c4f69;
     # @define-color base06 #dc8a78; @define-color base07 #7287fd;
@@ -20,64 +20,85 @@
     # @define-color base0A #df8e1d; @define-color base0B #40a02b;
     # @define-color base0C #179299; @define-color base0D #1e66f5;
     # @define-color base0E #8839ef; @define-color base0F #dd7878;
-    style = ''
-      * {
-          border: none;
-          border-radius: 0;
-          font-family: JetBrainsMono Nerd Font;
-          font-size: 13px;
-          min-height: 0;
-      }
+    style =
+      #css
+      ''
+        * {
+            border: none;
+            border-radius: 0;
+            font-family: JetBrainsMono Nerd Font;
+            font-size: 13px;
+            min-height: 0;
+        }
 
-      window#waybar {
-          background: alpha(@base00, 0.0);
+        window#waybar {
+            background: alpha(@base, 0.0);
 
-          margin: 5px 10px; /* Adds 5px margin top/bottom,
-                            10px left/right. Adjust values as needed. */
-          border-radius: 10px; /* Rounds all corners with a 10px radius.
-                                Adjust value as needed. */
-          /* Optional: Add a subtle shadow for more depth */
-          box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3); /* Example shadow */
-      }
-      #clock {
-          padding: 0 0.5em;
-          margin: 0.25em;
-          border-radius: 5px;
-          background: @base08;
-      }
-      #wireplumber {
-          padding: 0 0.5em;
-          margin: 0.25em;
-          border-radius: 5px;
-      }
-      #bluetooth {
-          padding: 0 0.5em;
-          margin: 0.25em;
-          border-radius: 5px;
-      }
-      #workspaces {
-          padding: 0 0.5em;
-          margin: 0.25em;
-          border-radius: 5px;
-      }
-      #workspaces button {
-          padding: 0em 0.55em 0em 0.2em;
-          margin: 0.25em;
-          border-radius: 5px;
-      }
-      #workspaces button.active {
-          padding: 0em 0.55em 0em 0.2em;
-          margin: 0.25em;
-          border-radius: 5px;
-      }
-      #mpris {
-          padding: 0 0.5em;
-          margin: 0.25em;
-          border-radius: 5px;
-          background: @base0A;
-          color: @base00;
-      }
-    '';
+            margin: 5px 10px; /* Adds 5px margin top/bottom,
+                              10px left/right. Adjust values as needed. */
+            border-radius: 10px; /* Rounds all corners with a 10px radius.
+                                  Adjust value as needed. */
+            /* Optional: Add a subtle shadow for more depth */
+            /* box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3); */
+        }
+        #clock {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @red;
+            color: @base;
+        }
+        #wireplumber {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @peach;
+        }
+        #wireplumber, #pulseaudio, #sndio {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @peach;
+            color: @base;
+        }
+        #wireplumber.muted, #pulseaudio.muted, #sndio.muted {
+            background: @teal;
+        }
+        #bluetooth {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @mauve;
+            color: @base;
+        }
+        #bluetooth.disabled {
+            background: @teal;
+        }
+        #workspaces {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+        }
+        #workspaces button {
+            padding: 0em 0.55em 0em 0.2em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @lavender;
+        }
+        #workspaces button.active,  #workspaces button.focused {
+            background: @surface1;
+        }
+        #workspaces button.urgent {
+          background: @red;
+        }
+        #mpris {
+            padding: 0 0.5em;
+            margin: 0.25em;
+            border-radius: 5px;
+            background: @green;
+            color: @base;
+        }
+      '';
     settings = {
       mainBar = {
         # output = ["DP-2"];
